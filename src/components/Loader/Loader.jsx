f6b7e901f8cc159ceb336a77573ea9e284b78bae
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-07 22:03:53 +0400

    some

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
   
   return (
     <div className="sweet-loading">
-        <FadeLoader color="#08449e" radius={'50%'} width={'30px'} height={'30px'} margin={40} />
+        <FadeLoader color="#08449e" radius={'50%'} width={'30px'} height={'30px'} margin={40}  cssOverride={{display:'flex'}}/>
     </div>
   );
 };
