c50333f26dfd6785f6d768e679e066ff0824d8a2
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-08 00:56:20 +0400

    some

--- src/components/ComicsDetails/ComicsDetails.jsx
+++ src/components/ComicsDetails/ComicsDetails.jsx
@@ -10,57 +10,30 @@ const ComicsDetails = () => {
   const { id } = useParams();
   const { comicsData } = useContext(ContextValue);
   const comics = comicsData.find((comics) => comics.id.toString() === id);
- 
+
   return (
     <div className="comics_details">
       <HeaderBanner />
 
-        {
-          comics ? (
-            <div className="comics_details_container">
-                <div className="comics__img__container">
-                <img src={comics.thumbnail.path + ".jpg"} alt="" />
-                </div>
-                <div className="comics__description__container">
-                  <div className="title_bold_black_22">
-                    <h4>{comics.title}</h4>
-                  </div>
-                  <div className="paragraph_18">
-                    <p >{comics.description}</p>
-                  </div>
-                  <div className="pargraph_18">
-                    <p>{comics.pageCount >0 ? comics.pageCount : 'No' } pages</p>
-                  </div>
-                  <div className="pargraph_18">
-                    <p>Language:{comics.textObjects.language}</p>
-                  </div>
-                  <div className="description_price">
-                  {comics.prices[0].price > 0 ? (
-                    <span>{comics.prices[0].price}$</span>
-                  ) : (
-                    <span>NOT AVAILABLE </span>
-                  )}
-                 </div>
-                </div>
-                <Link to={"/comics"} className="back_to_all title_bold_18">
-            Back to all
-          </Link>
-            </div>
-          ) : <Loader/>
-        }
-
-
-      {/* {comics ? (
-        <div className="comics-details-container">
-          <div className="comics-details-img">
+      {comics ? (
+        <div className="comics_details_container">
+          <div className="comics__img__container">
             <img src={comics.thumbnail.path + ".jpg"} alt="" />
           </div>
-          <div className="comics-details-description">
-            <h1>{comics.title}</h1>
-            <p>{comics.description}</p>
-            <h3>{comics.pageCount} pages</h3>
-            <h3>Language:{comics.textObjects.language}</h3>
-            <div className="comics_price">
+          <div className="comics__description__container">
+            <div className="title_bold_black_22">
+              <h4>{comics.title}</h4>
+            </div>
+            <div className="paragraph_18">
+              <p>{comics.description}</p>
+            </div>
+            <div className="pargraph_18">
+              <p>{comics.pageCount > 0 ? comics.pageCount : "No"} pages</p>
+            </div>
+            <div className="pargraph_18">
+              <p>Language:{comics.textObjects.language}</p>
+            </div>
+            <div className="description_price">
               {comics.prices[0].price > 0 ? (
                 <span>{comics.prices[0].price}$</span>
               ) : (
@@ -68,13 +41,13 @@ const ComicsDetails = () => {
               )}
             </div>
           </div>
-          <Link to={"/comics"} className="back-to-all">
+          <Link to={"/comics"} className="back_to_all title_bold_18">
             Back to all
           </Link>
         </div>
       ) : (
         <Loader />
-      )} */}
+      )}
     </div>
   );
 };
